47127dc3d47e23fcb50c00cea30d2a7063809602
Thais Silva <83255298+thaiscode@users.noreply.github.com>
2025-11-12 20:40:27 -0300

    update

---
+++ src/About.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./About.css";
+
+const About = () => {
+  return (
+    <section className="about">
+    <h2>A Adotfy faz a ponte <br/> entre você e a ONG mais próxima</h2>
+    <div className="about__item flex">
+        <div className="about__box">
+            <img src="/assets/images/ONGs_icon.png" alt="" />
+            <h3>ONGs</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+
+        <div className="about__box">
+            <img src="/assets/images/logo_icon_blue.png" alt="" />
+            <h3>Adotfy</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+
+        <div className="about__box">
+            <img src="/assets/images/interessados_adoção_icon.png" alt="" />
+            <h3>Interessados em adoção</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+    </div>    
+    </section>
+  );
+};
+
+export default About;
---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import '../style.css';
+
+import NavBar from "./NavBar.jsx";
+import About from "./About.jsx";
+import Pets from "./Pets.jsx";
+import ONG from "./ONG.jsx";
+import Perdidos from "./Perdidos.jsx";
+import Footer from "./footer.jsx";
+
+function renderIfExists(id, component) {
+  const el = document.getElementById(id);
+  if (el) {
+    createRoot(el).render(component);
+  }
+}
+
+renderIfExists("nav-root", <NavBar/>);
+renderIfExists("about-root", <About/>);
+renderIfExists("pets-root", <Pets/>);
+renderIfExists("ong-root", <ONG/>);
+renderIfExists("perdidos-root", <Perdidos/>);
+renderIfExists("footer-root", <Footer/>);
